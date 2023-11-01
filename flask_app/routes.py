f2f4c43505f5bb31d7b633614787fa828fe36627
livio-24 <vona.livio@gmail.com>
2023-11-01 20:03:46 +0100

    flask app final version

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,6 +1,8 @@
+import datetime
 import pprint
 from flask_app import app, db
-from flask import render_template, request, jsonify
+from flask import render_template, request, jsonify, redirect, url_for
+from bson import ObjectId
 
 
 @app.route("/")
@@ -8,10 +10,8 @@ def index():
     pipeline = [
         {
             "$group": {
-                "_id": "$name",  # Group by this attribute
-                "average_value": {
-                    "$avg": "$reviewsRating"
-                },  # Calculate the average of this attribute
+                "_id": "$name",  # Group by name
+                "average_value": {"$avg": "$reviewsRating"},
                 "hotel_info": {
                     "$first": {
                         "city": "$city",
@@ -21,23 +21,27 @@ def index():
                     }
                 },
             },
-        }
+        },
+        {
+            "$sort": {"average_value": -1},  # descending
+        },
     ]
 
-    aggregated_data = list(db.review.aggregate(pipeline))
+    aggregated_data = db.review.aggregate(pipeline)
 
     return render_template("index.html", hotels=aggregated_data)
 
 
-@app.route("/reviews/<string:hotel_id>")
-def show_reviews(hotel_id):
-    reviews = list(db.review.find({"name": hotel_id}))
+@app.route("/reviews/<string:hotel_name>")
+def show_reviews(hotel_name):
+    reviews = db.review.find({"name": hotel_name}).sort("reviewsDate", -1)
     return render_template("reviews.html", reviews=reviews)
 
 
 @app.route("/search")
 def search():
     search_attr = request.args.get("search_attr")
+    sort_order = int(request.args.get("sort_order"))
     query = request.args.get("query")  # La query di ricerca inviata dalla search bar
 
     pipeline = [
@@ -51,10 +55,8 @@ def search():
         },
         {
             "$group": {
-                "_id": "$name",  # Group by this attribute
-                "average_value": {
-                    "$avg": "$reviewsRating"
-                },  # Calculate the average of this attribute
+                "_id": "$name",
+                "average_value": {"$avg": "$reviewsRating"},
                 "hotel_info": {
                     "$first": {
                         "city": "$city",
@@ -65,8 +67,84 @@ def search():
                 },
             },
         },
+        {
+            "$sort": {"average_value": sort_order},
+        },
     ]
 
     risultati = list(db.review.aggregate(pipeline))
-    # risultati = list(db.review.find({"name": {"$regex": query, "$options": "i"}}))
     return jsonify(risultati)
+
+
+@app.route("/add/<string:hotel_name>", methods=["POST", "GET"])
+def add_review(hotel_name):
+    review = db.review.find_one({"name": hotel_name})
+    if request.method == "POST":
+        title = request.form["title"]
+        text = request.form["text"]
+        rating = int(request.form["rating"])
+        # Ottieni la data corrente
+        current_date = datetime.datetime.now()
+        # Formatta la data nel formato "anno-mese-giorno"
+        formatted_date = current_date.strftime("%Y-%m-%d")
+
+        db.review.insert_one(
+            {
+                "name": review["name"],
+                "city": review["city"],
+                "address": review["address"],
+                "postalCode": review["postalCode"],
+                "province": review["province"],
+                "reviewsTitle": title,
+                "reviewsText": text,
+                "reviewsRating": rating,
+                "reviewsUsername": "user_test",
+                "reviewsDate": formatted_date,
+            }
+        )
+
+        return redirect(url_for("index"))
+
+    return render_template("add_review.html", review=review)
+
+
+@app.route("/myreviews")
+def my_reviews():
+    reviews = db.review.find({"reviewsUsername": "user_test"}).sort("reviewsDate", -1)
+    return render_template("myreviews.html", reviews=reviews)
+
+
+@app.route("/delete/<string:hotel_id>")
+def delete_review(hotel_id):
+    hotel_id = ObjectId(hotel_id)
+    db.review.delete_one({"_id": hotel_id})
+    return redirect(url_for("my_reviews"))
+
+
+@app.route("/update/<string:hotel_id>", methods=["POST", "GET"])
+def update_review(hotel_id):
+    if request.method == "GET":
+        hotel_id = ObjectId(hotel_id)
+        review = db.review.find_one({"_id": hotel_id})
+        return render_template("update_review.html", review=review)
+    else:
+        title = request.form["title"]
+        text = request.form["text"]
+        rating = int(request.form["rating"])
+        # Ottieni la data corrente
+        current_date = datetime.datetime.now()
+        # Formatta la data nel formato "anno-mese-giorno"
+        formatted_date = current_date.strftime("%Y-%m-%d")
+        hotel_id = ObjectId(hotel_id)
+        review = db.review.update_one(
+            {"_id": hotel_id},
+            {
+                "$set": {
+                    "reviewsTitle": title,
+                    "reviewsText": text,
+                    "reviewsRating": rating,
+                    "reviewsDate": formatted_date,
+                }
+            },
+        )
+        return redirect(url_for("my_reviews"))
--- flask_app/static/index.js
+++ flask_app/static/index.js
@@ -1,24 +1,50 @@
 $(document).ready(function () {
-    $('#search').on('input', function () {
+    function handleInputChange() {
         var search_attr = $('#search-attribute').val();
-        var query = $(this).val();
-        $.get('/search', { query: query, search_attr: search_attr }, function (data) {
+        var query = $('#search').val();
+        var sort_order = ($('#sort-order').val() === 'asc') ? 1 : -1;
+        $.get('/search', { query: query, search_attr: search_attr, sort_order: sort_order }, function (data) {
             $('#results').empty();
             data.forEach(function (hotel) {
-                const html =
-                    `<div class="col">
-                        <div class="card">
+                var ratingInt = Math.floor(hotel.average_value);
+                var ratingDecimal = (hotel.average_value - ratingInt).toFixed(1);
+
+                var starsHTML = '';
+                for (var i = 0; i < ratingInt; i++) {
+                    starsHTML += '<i class="fas fa-star text-warning"></i>';
+                }
+                if (ratingDecimal >= 0.3 && ratingDecimal <= 0.7) {
+                    starsHTML += '<i class="fas fa-star-half-alt text-warning"></i>';
+                }
+                if (ratingDecimal >= 0.8) {
+                    starsHTML += '<i class="fas fa-star text-warning"></i>';
+                }
+
+                var hotelCardHTML = `
+                    <div class="col">
+                        <div class="card h-100">
+                            <h5 class="card-header">${hotel._id}</h5>
                             <div class="card-body">
-                                <h5 class="card-title">${hotel._id}</h5>
-                                <p class="card-text">Avg rating: ${hotel.average_value.toFixed(1)}</p>
-                                <p class="card-text">${hotel.hotel_info.city}, ${hotel.hotel_info.address} (${hotel.hotel_info.province})</p>
+                                ${starsHTML}
+                                <i>${hotel.average_value.toFixed(1)}</i>
+                                <p class="card-text text-dark-emphasis">${hotel.hotel_info.city}, ${hotel.hotel_info.address} (${hotel.hotel_info.province})</p>
                                 <a href="/reviews/${hotel._id}" class="btn btn-primary">View reviews</a>
+                                <a href="/add/${hotel._id}" class="btn btn-secondary">Add review</a>
                             </div>
                         </div>
-                    </div>`;
-                // Aggiungi il blocco HTML al tuo contenitore
-                $('#results').append(html);
+                    </div>
+                `;
+
+                // Aggiungi il blocco di codice HTML dell'hotel al contenitore
+                $('#results').append(hotelCardHTML);
             });
         });
-    });
+    }
+
+    $('#search-button').on('click', handleInputChange)
+    $('#search').on('input', handleInputChange);
+    $('#sort-order').on('change', handleInputChange);
+    $('#search-attribute').on('input', handleInputChange);
+
+
 });
